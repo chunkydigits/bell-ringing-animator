9b4aa0b8fd2c3d8bc938ad47c7a1ee8e1c739cbf
chunkydigits <adrb1984@gmail.com>
2019-06-06 21:34:46 +0100

    initial commit

---
+++ _vti_cnf/Animator.java
@@ -0,0 +1,8 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|02 May 2006 10:44:35 -0000
+vti_extenderversion:SR|5.0.2.6790
+vti_lineageid:SR|{0DCFF64A-DADA-4247-A38C-C2B34B9C64A3}
+vti_cacheddtm:TX|30 Apr 2006 13:35:43 -0000
+vti_filesize:IR|47443
+vti_backlinkinfo:VX|
+vti_modifiedby:SR|FUSIONBOB\\Andrew Brown
